24069a3ce9e1ed580148e5b1ac62173c156543bd
Karol <karol7525@gmail.com>
2024-07-27 18:03:02 +0200

    Added empty files

---
+++ sources/View/frmProjectMass.dfm
@@ -0,0 +1,14 @@
+object FormProjectMass: TFormProjectMass
+  Left = 0
+  Top = 0
+  Caption = 'FormProjectMass'
+  ClientHeight = 433
+  ClientWidth = 622
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  TextHeight = 15
+end
---
+++ sources/View/frmProjectMass.pas
@@ -0,0 +1,24 @@
+unit frmProjectMass;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TFormProjectMass = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FormProjectMass: TFormProjectMass;
+
+implementation
+
+{$R *.dfm}
+
+end.
